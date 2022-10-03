c069bc3ef5970cd352dd26b675523d3b4af03f69
Chris <buswinka@gmail.com>
2022-10-03 13:49:35 -0400

    added a new way to get a model constructor

---
+++ bism/models/__init__.py
@@ -0,0 +1,1 @@
+from load import get_constructor
---
+++ bism/models/load.py
@@ -0,0 +1,25 @@
+from bism.models.unet import UNet_2D, UNet_3D
+from bism.models.unext import UNeXT_2D, UNeXT_3D
+from bism.models.r_unet import RUNeT_2D, RUNeT_3D
+from bism.models.cellpose_net import CPnet_2D, CPnet_3D
+from bism.models.unetplusplus import UNetPlusPlus_3D, UNetPlusPlus_2D
+
+import torch.nn as nn
+import torch
+
+__available_models__ = ['unet', 'unet++', 'runet', 'cellpose_net']
+def get_constructor(model_name, spatial_dim: int = 2):
+    if spatial_dim not in [2, 3]:
+        raise RuntimeError(f'Spatial dimmension of {spatial_dim} is not supported')
+
+    constructors = {'unet2d': UNet_2D,
+                    'unet3d': UNet_3D,
+                    'unet++2d': UNetPlusPlus_2D,
+                    'unet++3d': UNetPlusPlus_3D,
+                    'runet2d': RUNeT_2D,
+                    'runet3d': RUNeT_3D,
+                    'cellpose_net2d': CPnet_2D,
+                    'cellpose_net3d': CPnet_3D,
+                    }
+
+    return constructors[f'model_name{spatial_dim}d']
